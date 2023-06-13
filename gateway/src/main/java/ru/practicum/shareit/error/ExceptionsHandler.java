48be66dda6e3972ce974def8696163bfbf1d87ad
Ekaterina <katya-3105@yandex.ru>
2023-06-13 22:03:30 +0300

    Валидация времени бронирования вынесена в gateway, поправлены тесты

--- gateway/src/main/java/ru/practicum/shareit/error/ExceptionsHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/error/ExceptionsHandler.java
@@ -1,16 +1,11 @@
 package ru.practicum.shareit.error;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.server.ServerWebExchange;
-
-import java.nio.charset.StandardCharsets;
 
 @RestControllerAdvice
 @Slf4j
